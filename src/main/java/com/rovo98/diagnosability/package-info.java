10ae8ff10eb38858195a197c05d64dc52e9cd5f6
rovo98 <rovo98@qq.com>
2020-06-08 00:44:20 +0800

    update: add package infos.

---
+++ src/main/java/com/rovo98/diagnosability/package-info.java
@@ -0,0 +1,6 @@
+/**
+ * Provides default Diagnoser implementation for testing diagnosability of generated dfas in polynomial time.
+ *
+ * @author rovo98
+ */
+package com.rovo98.diagnosability;
---
+++ src/main/java/com/rovo98/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * Provides basic support classes and annotations for generating DFAs.
+ *
+ * @author rovo98
+ * @since 1.0
+ */
+package com.rovo98;
